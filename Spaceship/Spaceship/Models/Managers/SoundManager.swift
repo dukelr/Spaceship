2c944a85476971b8df04c84006f347449d755214
Nikolai Duke <125504340+dukelr@users.noreply.github.com>
2023-02-20 21:29:36 -0500

    SS. Added sounds

--- Spaceship/Spaceship/Models/Managers/SoundManager.swift
+++ Spaceship/Spaceship/Models/Managers/SoundManager.swift
@@ -1,8 +1,42 @@
-//
-//  SoundManager.swift
-//  Spaceship
-//
-//  Created by duke on 2/20/23.
-//
 
 import Foundation
+import AVFoundation
+
+private extension String {
+    static let soundType = "mp3"
+}
+
+final class SoundManager {
+    
+    static let shared = SoundManager()
+    
+    private var player: AVAudioPlayer?
+    
+    enum SoundName: String {
+        case spaceship = "sound_spaceship"
+        case laser = "sound_laser"
+        case button = "sound_button"
+    }
+    
+    func playSound(_ soundName: SoundName, repeats: Bool = false) {
+        guard let fileName = Bundle.main.path(forResource: soundName.rawValue, ofType: .soundType) else { return }
+        
+        let url = URL(fileURLWithPath: fileName)
+        try? AVAudioSession.sharedInstance().setCategory(.playback)
+        try? AVAudioSession.sharedInstance().setActive(true)
+        player = try? AVAudioPlayer(contentsOf: url)
+        player?.volume = 1
+        player?.prepareToPlay()
+        player?.play()
+        
+        if repeats {
+            player?.numberOfLoops = -1
+        }
+    }
+    
+    func stopSound() {
+//        players?.forEach { player in
+            player?.stop()
+//        }
+    }
+}
